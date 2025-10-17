68680afa1d11736aa79a66b61d4c91fea310032a
杨文华 <1289978696@qq.com>
2025-10-17 11:32:16 +0800

    feat: 完善侧边栏

--- app/home/components/NavigationBar/index.tsx
+++ app/home/components/NavigationBar/index.tsx
@@ -1,8 +1,101 @@
-import { IconGork, IconX } from "@/components/features/icon";
+import { IconBookMarks, IconCommunitity, IconGork, IconHome, IconList, IconMessage, IconMore, IconNotification, IconPersonal, IconPost, IconSearch, IconX } from "@/components/features/icon";
 
 export default function NavigationBar() {
-    return <div className="flex-[0.45] bg-gray-100">
-        <IconX />
-        <IconGork />
+    return <div className="flex-[0.45] flex justify-end ">
+        <div className="w-[275px] box-border px-4 py-4">
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconX />
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconHome />
+                    <span className="text-[16px] font-medium">
+                        主页
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconSearch />
+                    <span>
+                        探索
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconNotification />
+                    <span>
+                        通知
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconMessage />
+                    <span>
+                        私信
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconGork />
+                    <span>
+                        Grok
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconList />
+                    <span>
+                        列表
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconBookMarks />
+                    <span>
+                        书签
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconCommunitity />
+                    <span>
+                        社群
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconPersonal />
+                    <span>
+                        个人资料
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconMore />
+                    <span>
+                        更多
+                    </span>
+                </div>
+            </div>
+            <div className="w-[245px] cursor-pointer">
+                <div className="w-[166px] h-[50px] flex items-center pl-5  gap-5 hover:bg-gray-200 rounded-full">
+                    <IconPost />
+                    <span>
+                        发帖
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
 }
